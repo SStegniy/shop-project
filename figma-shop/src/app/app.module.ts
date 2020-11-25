f317d64cae964513fa96be16e3e70875f0283d64
SStegniy <sstegniy@reenbit.com>
2020-11-25 09:49:45 +0200

    start

--- figma-shop/src/app/app.module.ts
+++ figma-shop/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderTopComponent } from './components/header/header-top/header-top.component';
+import { HeaderNavComponent } from './components/header/header-nav/header-nav.component';
+import { HeaderBreadcrumbsComponent } from './components/header/header-breadcrumbs/header-breadcrumbs.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderTopComponent,
+    HeaderNavComponent,
+    HeaderBreadcrumbsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ figma-shop/src/app/components/header/header-breadcrumbs/header-breadcrumbs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-breadcrumbs',
+  templateUrl: './header-breadcrumbs.component.html',
+  styleUrls: ['./header-breadcrumbs.component.scss']
+})
+export class HeaderBreadcrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
