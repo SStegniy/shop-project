e03916ed4e70a792b450686fe6a1b2bc23c7fc48
SStegniy <sstegniy@reenbit.com>
2020-12-17 10:49:40 +0200

    start UI of product details

--- figma-shop/src/app/pages/product-details/product-details.component.ts
+++ figma-shop/src/app/pages/product-details/product-details.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ProductService } from '../../shared/services/product.service';
 import { ProductInterface } from '../../shared/interfaces/product.interface';
 
 @Component({
@@ -10,21 +9,19 @@ import { ProductInterface } from '../../shared/interfaces/product.interface';
 })
 
 export class ProductDetailsComponent implements OnInit {
-  private product: ProductInterface;
+  public product: ProductInterface;
   public starCount = [1, 2, 3, 4, 5];
 
   constructor(
-    private actRoute: ActivatedRoute,
-    private productService: ProductService) { }
+    private actRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.actRoute.data.subscribe((data) => {
-      this.product = data.product;
-    });
+    this.getProduct();
   }
 
   private getProduct(): void {
-    const id = +this.actRoute.snapshot.params.id;
-    this.productService.getOneProduct(id).then(data => this.product = data);
+    this.actRoute.data.subscribe((data) => {
+      this.product = data.product;
+    });
   }
 }
