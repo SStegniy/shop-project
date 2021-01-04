fa000bdd9e0a4af8b2dc4de197a0b840d35fcc6a
SStegniy <sstegniy@reenbit.com>
2021-01-04 09:47:23 +0200

    add condition to buy from wish list

--- figma-shop/src/app/pages/wish-list/wish-list.component.ts
+++ figma-shop/src/app/pages/wish-list/wish-list.component.ts
@@ -3,6 +3,8 @@ import { ProductInterface } from '../../shared/interfaces/product.interface';
 import { WishListService } from '../../shared/services/wish-list.service';
 import { OrderService } from '../../shared/services/order.service';
 import { SnackbarService } from '../../shared/services/snackbar.service';
+import { UserInterface } from '../../shared/interfaces/user.interface';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-wish-list',
@@ -11,17 +13,24 @@ import { SnackbarService } from '../../shared/services/snackbar.service';
 })
 export class WishListComponent implements OnInit {
   public products: ProductInterface[];
+  public user: UserInterface;
   constructor(
     private wishService: WishListService,
     private orderService: OrderService,
+    private authService: AuthService,
     private snackbarService: SnackbarService) { }
 
   ngOnInit(): void {
     this.products = this.getWishedProducts();
+    this.user = this.getUser();
   }
 
   private getWishedProducts(): ProductInterface[] {
     return this.wishService.getWishedProducts();
+  }
+
+  private getUser(): UserInterface {
+    return this.authService.getUserFromLocalStorage();
   }
 
   public removeProduct(id: number, index: number): void {
@@ -30,8 +39,13 @@ export class WishListComponent implements OnInit {
   }
 
   public addToCart(product: ProductInterface): void {
-    this.snackbarService.snackMessage('Add to Cart', true);
-    this.orderService.setOrderToLocalStorage(product);
+    if (this.user) {
+      this.snackbarService.snackMessage('Add to Cart', true);
+      this.orderService.setOrderToLocalStorage(product);
+    }
+    else {
+      this.snackbarService.snackMessage('Please log in', false);
+    }
   }
 
 }
